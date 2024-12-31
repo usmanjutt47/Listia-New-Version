d610096a443e0f29c15cc627ad983fe11d08cf2b
Muhammad Usman <142773789+usmanjutt47@users.noreply.github.com>
2024-12-31 15:19:12 +0500

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{ headerShown: false }}
+      initialRouteName="(onboarding)/firstScreen/index"
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="(onboarding)/firstScreen/index" />
+      <Stack.Screen name="(onboarding)/secondScreen/index" />
+      <Stack.Screen name="(onboarding)/thirdScreen/index" />
+    </Stack>
+  );
 }
