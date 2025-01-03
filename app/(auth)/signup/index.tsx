c59653158b61957143ba8c7c7949b37f1898225c
Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2025-01-03 09:47:25 +0500

    Update index.tsx

--- app/(auth)/signup/index.tsx
+++ app/(auth)/signup/index.tsx
@@ -1,10 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { router } from "expo-router";
 
-export default function index() {
-    return (
-        <View>
-            <Text>index</Text>
-        </View>
-    )
+export default function SignUp() {
+  const handle = () => {
+    router.push("/login");
+  };
+  return (
+    <View style={styles.container}>
+      <Text>SignUp Screen </Text>
+      <TouchableOpacity style={styles.btn} onPress={handle}>
+        <Text style={styles.txt}>SignUp</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btn: {
+    width: "90%",
+    height: 50,
+    marginTop: 20,
+    justifyContent: "center",
+    alignSelf: "center",
+    backgroundColor: "#238832",
+    borderRadius: 15,
+    alignItems: "center",
+  },
+  txt: {
+    fontSize: 16,
+    color: "#fff",
+  },
+});
