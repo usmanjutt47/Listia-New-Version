9720e26963f564b640f19d3cb4a123b6e2adf516
Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2025-01-03 09:47:22 +0500

    Update index.tsx

--- app/(auth)/login/index.tsx
+++ app/(auth)/login/index.tsx
@@ -1,79 +1,25 @@
-import React, { useState } from "react";
-import {
-  View,
-  StyleSheet,
-  StatusBar,
-  Image,
-  TextInput,
-  ScrollView,
-} from "react-native";
-import Feather from "@expo/vector-icons/Feather";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { router } from "expo-router";
 
 export default function Login() {
-  const [activeInput, setActiveInput] = useState<string | null>(null);
+  const handle = () => {
+    router.push("/home");
+  };
+
+  const handleForgot = () => {
+    router.push("/forgot-password");
+  };
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" />
-      <View style={styles.backgroundContainer}>
-        <Image
-          source={require("../../../assets/images/login.png")}
-          style={styles.image}
-        />
-      </View>
-      <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        <View style={styles.innerContainer}>
-          <View
-            style={[
-              styles.inputContainer,
-              activeInput === "email"
-                ? styles.activeInput
-                : styles.inactiveInput,
-            ]}
-          >
-            <Feather
-              name="mail"
-              size={24}
-              color={activeInput === "email" ? "#2DA144" : "#838383"}
-              style={styles.icon}
-            />
-            <TextInput
-              placeholder="Email Address"
-              onFocus={() => setActiveInput("email")}
-              onBlur={() => setActiveInput(null)}
-              style={styles.textInput}
-              placeholderTextColor={
-                activeInput === "email" ? "#2DA144" : "#838383"
-              }
-            />
-          </View>
-          <View
-            style={[
-              styles.inputContainer,
-              activeInput === "password"
-                ? styles.activeInput
-                : styles.inactiveInput,
-            ]}
-          >
-            <FontAwesome
-              name="unlock-alt"
-              size={24}
-              color={activeInput === "password" ? "#2DA144" : "#838383"}
-              style={styles.icon}
-            />
-            <TextInput
-              placeholder="Password"
-              onFocus={() => setActiveInput("password")}
-              onBlur={() => setActiveInput(null)}
-              style={styles.textInput}
-              placeholderTextColor={
-                activeInput === "password" ? "#2DA144" : "#838383"
-              }
-            />
-          </View>
-        </View>
-      </ScrollView>
+      <Text>Login Screen </Text>
+      <TouchableOpacity style={styles.btn} onPress={handle}>
+        <Text style={styles.txt}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleForgot} style={{ marginTop: 50 }}>
+        <Text>Forgot your password?</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -81,52 +27,21 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-  },
-  scrollViewContent: {
-    flexGrow: 1,
-  },
-  backgroundContainer: {
-    backgroundColor: "#2DA144",
-    width: "100%",
-    height: "45%",
-    borderBottomLeftRadius: 86,
-    borderBottomRightRadius: 86,
-  },
-  image: {
-    width: "90%",
-    height: "100%",
-    resizeMode: "contain",
-    alignSelf: "center",
-  },
-  innerContainer: {
-    paddingTop: 30,
-    width: "90%",
-    alignSelf: "center",
-    backgroundColor: "#fff",
-    height: "100%",
-  },
-  inputContainer: {
-    paddingLeft: 10,
-    borderRadius: 48,
-    width: "100%",
-    marginTop: "3%",
-    height: 48,
-    flexDirection: "row",
+    justifyContent: "center",
     alignItems: "center",
   },
-  activeInput: {
-    borderWidth: 1,
-    borderColor: "#2DA144",
+  btn: {
+    width: "90%",
+    height: 50,
+    justifyContent: "center",
+    alignSelf: "center",
+    backgroundColor: "#238832",
+    borderRadius: 15,
+    alignItems: "center",
+    marginTop: 10,
   },
-  inactiveInput: {
-    backgroundColor: "#F2F2F2",
-  },
-  textInput: {
-    flex: 1,
-    height: 40,
-  },
-  icon: {
-    marginRight: 10,
+  txt: {
+    fontSize: 16,
+    color: "#fff",
   },
 });
