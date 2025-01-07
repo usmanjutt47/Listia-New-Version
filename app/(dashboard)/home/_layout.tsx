7e3c176da2e350077e956c4291072fb8a155da45
Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2025-01-07 09:09:06 +0500

    Update _layout.tsx

--- app/(dashboard)/home/_layout.tsx
+++ app/(dashboard)/home/_layout.tsx
@@ -1,36 +1,17 @@
 import React from "react";
 import { Drawer } from "expo-router/drawer";
 import { Ionicons } from "@expo/vector-icons";
+import CustomDrawerContent from "@/components/CustomDrawerContent";
 
 export default function DrawerComponent() {
   return (
-    <Drawer
-      screenOptions={{
-        drawerStyle: {
-          backgroundColor: "#238A34",
-          borderTopRightRadius: 0,
-          borderBottomRightRadius: 0,
-        },
-        drawerActiveTintColor: "#FDFDFD",
-        drawerInactiveTintColor: "#333",
-        drawerActiveBackgroundColor: "#333",
-        drawerHideStatusBarOnOpen: true,
-        drawerStatusBarAnimation: "slide",
-        drawerItemStyle: {
-          borderRadius: 0,
-        },
-      }}
-    >
+    <Drawer drawerContent={CustomDrawerContent}>
       <Drawer.Screen
         name="index"
         options={{
           drawerLabel: "Home",
-          drawerIcon: ({ focused, size }) => (
-            <Ionicons
-              name="home"
-              size={size}
-              color={focused ? "#FDFDFD" : "#333"}
-            />
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
           ),
         }}
       />
@@ -38,12 +19,8 @@ export default function DrawerComponent() {
         name="CompleteList"
         options={{
           drawerLabel: "Complete List",
-          drawerIcon: ({ focused, size }) => (
-            <Ionicons
-              name="list"
-              size={size}
-              color={focused ? "#FDFDFD" : "#333"}
-            />
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" size={size} color={color} />
           ),
         }}
       />
@@ -51,12 +28,8 @@ export default function DrawerComponent() {
         name="Profile"
         options={{
           drawerLabel: "Profile",
-          drawerIcon: ({ focused, size }) => (
-            <Ionicons
-              name="person"
-              size={size}
-              color={focused ? "#FDFDFD" : "#333"}
-            />
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
           ),
         }}
       />
@@ -64,12 +37,8 @@ export default function DrawerComponent() {
         name="Subscription"
         options={{
           drawerLabel: "Subscription",
-          drawerIcon: ({ focused, size }) => (
-            <Ionicons
-              name="card"
-              size={size}
-              color={focused ? "#FDFDFD" : "#333"}
-            />
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="card" size={size} color={color} />
           ),
         }}
       />
@@ -77,12 +46,8 @@ export default function DrawerComponent() {
         name="ContactUs"
         options={{
           drawerLabel: "Contact Us",
-          drawerIcon: ({ focused, size }) => (
-            <Ionicons
-              name="call"
-              size={size}
-              color={focused ? "#FDFDFD" : "#333"}
-            />
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="call" size={size} color={color} />
           ),
         }}
       />
